5daeb909f26a884dc4ec3c2af08f4f3195f51ed3
LandonWong <LandonWong@github.com>
2020-06-23 18:59:49 +0800

    Update: memcpy_asm

--- memcpy/memcpy.c
+++ memcpy/memcpy.c
@@ -1,6 +1,6 @@
 #include <stddef.h>
 void *
 memcpy(void *dest, const void *src, size_t n){
-	(int*)dest = (int*)src;
+	dest = (int *)src;
 	return dest;
 }
--- memcpy/memcpy.s
+++ memcpy/memcpy.s
@@ -1,1 +1,14 @@
 	.file	"memcpy.c"
+	.text
+	.globl	memcpy
+	.type	memcpy, @function
+memcpy:
+.LFB0:
+	.cfi_startproc
+	movl	8(%esp), %eax
+	ret
+	.cfi_endproc
+.LFE0:
+	.size	memcpy, .-memcpy
+	.ident	"GCC: (Ubuntu 7.5.0-3ubuntu1~18.04) 7.5.0"
+	.section	.note.GNU-stack,"",@progbits
