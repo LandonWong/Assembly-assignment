e509a93c3e20bd13bb669dfd797a292c2b8f454c
LandonWong <LandonWong@github.com>
2020-06-24 08:20:20 +0800

    Update: atoi_clean

--- atoi/atoi.s
+++ atoi/atoi.s
@@ -1,27 +1,17 @@
-	.file	"atoi.c"
 	.text
 	.p2align 4,,15
 	.globl	myatoi
 	.type	myatoi, @function
 myatoi:
 .LFB0:
-	.cfi_startproc
 	pushl	%edi
-	.cfi_def_cfa_offset 8
-	.cfi_offset 7, -8
 	pushl	%esi
-	.cfi_def_cfa_offset 12
-	.cfi_offset 6, -12
 	pushl	%ebx
-	.cfi_def_cfa_offset 16
-	.cfi_offset 3, -16
 	movl	16(%esp), %edx
 	movzbl	(%edx), %ecx
 	leal	-9(%ecx), %eax
 	cmpb	$1, %al
 	ja	.L20
-	.p2align 4,,10
-	.p2align 3
 .L17:
 	addl	$1, %edx
 	movzbl	(%edx), %ecx
@@ -45,8 +35,6 @@ myatoi:
 	cmpb	$9, %bl
 	jbe	.L9
 	jmp	.L23
-	.p2align 4,,10
-	.p2align 3
 .L8:
 	leal	(%eax,%eax,4), %eax
 	leal	-48(%ecx,%eax,2), %eax
@@ -60,36 +48,16 @@ myatoi:
 	jbe	.L8
 	imull	%edi, %eax
 	popl	%ebx
-	.cfi_remember_state
-	.cfi_restore 3
-	.cfi_def_cfa_offset 12
 	popl	%esi
-	.cfi_restore 6
-	.cfi_def_cfa_offset 8
 	popl	%edi
-	.cfi_restore 7
-	.cfi_def_cfa_offset 4
 	ret
-	.p2align 4,,10
-	.p2align 3
 .L13:
-	.cfi_restore_state
 	popl	%ebx
-	.cfi_remember_state
-	.cfi_restore 3
-	.cfi_def_cfa_offset 12
 	movl	$2147483647, %eax
 	popl	%esi
-	.cfi_restore 6
-	.cfi_def_cfa_offset 8
 	popl	%edi
-	.cfi_restore 7
-	.cfi_def_cfa_offset 4
 	ret
-	.p2align 4,,10
-	.p2align 3
 .L22:
-	.cfi_restore_state
 	addl	$1, %edx
 	movsbl	(%edx), %ecx
 	movl	$-1, %edi
@@ -100,18 +68,7 @@ myatoi:
 	jbe	.L9
 .L23:
 	popl	%ebx
-	.cfi_restore 3
-	.cfi_def_cfa_offset 12
 	xorl	%eax, %eax
 	popl	%esi
-	.cfi_restore 6
-	.cfi_def_cfa_offset 8
 	popl	%edi
-	.cfi_restore 7
-	.cfi_def_cfa_offset 4
 	ret
-	.cfi_endproc
-.LFE0:
-	.size	myatoi, .-myatoi
-	.ident	"GCC: (Ubuntu 7.5.0-3ubuntu1~18.04) 7.5.0"
-	.section	.note.GNU-stack,"",@progbits
