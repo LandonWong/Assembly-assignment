bdb5af28fc3107ae78f11868d5e3cddd315535fb
LandonWong <LandonWong@github.com>
2020-06-24 08:12:19 +0800

    Update: atoi_clean

--- atoi/atoi.s
+++ atoi/atoi.s
@@ -1,16 +1,10 @@
-	.file	"atoi.c"
 	.text
 	.globl	myatoi
 	.type	myatoi, @function
 myatoi:
 .LFB0:
-	.cfi_startproc
 	pushl	%esi
-	.cfi_def_cfa_offset 8
-	.cfi_offset 6, -8
 	pushl	%ebx
-	.cfi_def_cfa_offset 12
-	.cfi_offset 3, -12
 	movl	12(%esp), %edx
 	movzbl	(%edx), %ecx
 	leal	-9(%ecx), %eax
@@ -50,23 +44,12 @@ myatoi:
 .L8:
 	imull	%esi, %eax
 	popl	%ebx
-	.cfi_remember_state
-	.cfi_restore 3
-	.cfi_def_cfa_offset 8
 	popl	%esi
-	.cfi_restore 6
-	.cfi_def_cfa_offset 4
 	ret
 .L15:
-	.cfi_restore_state
 	addl	$1, %edx
 	movl	$-1, %esi
 	jmp	.L6
 .L16:
 	addl	$1, %edx
 	jmp	.L6
-	.cfi_endproc
-.LFE0:
-	.size	myatoi, .-myatoi
-	.ident	"GCC: (Ubuntu 7.5.0-3ubuntu1~18.04) 7.5.0"
-	.section	.note.GNU-stack,"",@progbits
