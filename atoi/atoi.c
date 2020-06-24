55076c846c0b16a51578880988dd97ab5dd4afea
LandonWong <LandonWong@github.com>
2020-06-24 08:41:48 +0800

    Update: atoi_clean

--- atoi/atoi.c
+++ atoi/atoi.c
@@ -1,24 +1,4 @@
 int
 myatoi(const char *nptr){
-	int sign,result;
-	sign = 1;
-	result = 0;
-	while(*nptr == '\n' || *nptr == ' ' || *nptr == '\t')
-		nptr++;
-	if(*nptr == '-'){
-		sign = -1;
-		nptr++;
-	}else if(*nptr == '+'){
-		nptr++;
-	}
-	while(*nptr >= '0' && *nptr <= '9'){
-		result = result * 10;
-		result = result + *nptr - '0';
-		if(result > 0x70000){
-			result = 0x7fffffff;
-			goto L;
-		}
-		nptr++;
-	}
-L:	return sign * result;
+	return *nptr;
 }
