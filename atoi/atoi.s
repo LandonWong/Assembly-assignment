ad31a5ea3e352de12a2140006a6fbe6a0b2fa19f
LandonWong <LandonWong@github.com>
2020-06-24 10:25:36 +0800

    Update: atoi_asm

--- atoi/atoi.s
+++ atoi/atoi.s
@@ -3,6 +3,13 @@
 	.globl	myatoi
 	.type	myatoi, @function
 myatoi:
+
+# Description:
+# | edx | nptr			|
+# | ecx | load temp element	|
+# | ebx | sign (+1/-1)		|
+# | eax | return value (result) |
+
 .L0:
 	movl	4(%esp),%edx 	# base ptr
 	push	%ebx		# sign
@@ -24,35 +31,35 @@ myatoi:
 	inc	%edx
 	jmp	.L1
 .L2:				# L2: Read '+'/'-'
-	cmp	$'-',%cl
+	cmp	$'-',%cl	# set sign = -1
 	je	.L3
-	cmp	$'+',%cl
+	cmp	$'+',%cl	# sign has been already set to 1, just nptr++ 
 	je	.L4
-	jmp	.L5
-.L3:
+	jmp	.L5		# Cannot find
+.L3:				# set sign = -1
 	mov	$-1,%ebx
-.L4:
+.L4:				# nptr++
 	inc	%edx
-.L5:
-	movzx	(%edx),%ecx
-	cmp	$'0',%cl
+.L5:				# L5: <main loop>
+	movzx	(%edx),%ecx	# load element
+	cmp	$'0',%cl	# is not num
 	jl	.L8
-	cmp	$'9',%cl
+	cmp	$'9',%cl	# is not num
 	jg	.L8
-	imull	$10,%eax
-	jo	.L7
-	sub	$'0',%eax
-	add	%ecx,%eax
-	jo	.L7
-	inc	%edx
+	imull	$10,%eax	# result = result * 10
+	jo	.L7		# if OVERFLOW, goto OVERFLOW handle
+	sub	$'0',%eax	# sub '0'
+	add	%ecx,%eax	# add char
+	jo	.L7		# if OVERFLOW, goto OVERFLOW handle
+	inc	%edx		# nptr++
 	jmp	.L5
-.L7:
-	mov	$0x7fffffff,%eax
+.L7:				# L7: OVERFLOW handle
+	mov	$0x7fffffff,%eax# set value max
 	cmp	$1,%ebx
 	je	.L8
-	not	%eax
+	not	%eax		# if sign == -1, set value min
 .L8:
-	imull	%ebx,%eax
+	imull	%ebx,%eax	# result = result * sign
 	pop	%ecx
-	pop	%ebx
-	ret
+	pop	%ebx		# restore registers
+	ret			# return
