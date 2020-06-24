44dde50221fae8bd6610a4989e00316f8afff7aa
LandonWong <LandonWong@github.com>
2020-06-24 08:20:23 +0800

    Update: atoi_asm

--- atoi/atoi.s
+++ atoi/atoi.s
@@ -1,74 +1,123 @@
+	.file	"atoi.c"
 	.text
 	.p2align 4,,15
 	.globl	myatoi
 	.type	myatoi, @function
 myatoi:
 .LFB0:
+	.cfi_startproc
 	pushl	%edi
+	.cfi_def_cfa_offset 8
+	.cfi_offset 7, -8
 	pushl	%esi
+	.cfi_def_cfa_offset 12
+	.cfi_offset 6, -12
 	pushl	%ebx
-	movl	16(%esp), %edx
-	movzbl	(%edx), %ecx
-	leal	-9(%ecx), %eax
+	.cfi_def_cfa_offset 16
+	.cfi_offset 3, -16
+	movl	16(%esp), %ecx
+	movsbl	(%ecx), %edx
+	leal	-9(%edx), %eax
 	cmpb	$1, %al
 	ja	.L20
+	.p2align 4,,10
+	.p2align 3
 .L17:
-	addl	$1, %edx
-	movzbl	(%edx), %ecx
-	leal	-9(%ecx), %eax
+	addl	$1, %ecx
+	movsbl	(%ecx), %edx
+	leal	-9(%edx), %eax
 	cmpb	$1, %al
 	jbe	.L17
 .L20:
-	cmpb	$32, %cl
+	cmpb	$32, %dl
 	je	.L17
-	cmpb	$45, %cl
+	cmpb	$45, %dl
 	je	.L22
-	xorl	%eax, %eax
-	cmpb	$43, %cl
+	cmpb	$43, %dl
 	movl	$1, %edi
-	sete	%al
-	addl	%eax, %edx
-	movsbl	(%edx), %ecx
-	leal	-48(%ecx), %esi
-	leal	-48(%ecx), %eax
+	je	.L23
+.L6:
+	leal	-48(%edx), %esi
+	leal	-48(%edx), %eax
 	movl	%esi, %ebx
 	cmpb	$9, %bl
 	jbe	.L9
-	jmp	.L23
+	jmp	.L24
+	.p2align 4,,10
+	.p2align 3
 .L8:
 	leal	(%eax,%eax,4), %eax
-	leal	-48(%ecx,%eax,2), %eax
+	leal	-48(%edx,%eax,2), %eax
 	cmpl	$458752, %eax
 	jg	.L13
 .L9:
-	addl	$1, %edx
-	movsbl	(%edx), %ecx
-	leal	-48(%ecx), %ebx
+	addl	$1, %ecx
+	movsbl	(%ecx), %edx
+	leal	-48(%edx), %ebx
 	cmpb	$9, %bl
 	jbe	.L8
 	imull	%edi, %eax
 	popl	%ebx
+	.cfi_remember_state
+	.cfi_restore 3
+	.cfi_def_cfa_offset 12
 	popl	%esi
+	.cfi_restore 6
+	.cfi_def_cfa_offset 8
 	popl	%edi
+	.cfi_restore 7
+	.cfi_def_cfa_offset 4
 	ret
-.L13:
-	popl	%ebx
-	movl	$2147483647, %eax
-	popl	%esi
-	popl	%edi
-	ret
-.L22:
-	addl	$1, %edx
-	movsbl	(%edx), %ecx
-	movl	$-1, %edi
-	leal	-48(%ecx), %esi
-	leal	-48(%ecx), %eax
+	.p2align 4,,10
+	.p2align 3
+.L23:
+	.cfi_restore_state
+	movsbl	1(%ecx), %edx
+	addl	$1, %ecx
+	leal	-48(%edx), %esi
+	leal	-48(%edx), %eax
 	movl	%esi, %ebx
 	cmpb	$9, %bl
 	jbe	.L9
-.L23:
+.L24:
 	popl	%ebx
+	.cfi_remember_state
+	.cfi_restore 3
+	.cfi_def_cfa_offset 12
 	xorl	%eax, %eax
 	popl	%esi
+	.cfi_restore 6
+	.cfi_def_cfa_offset 8
 	popl	%edi
+	.cfi_restore 7
+	.cfi_def_cfa_offset 4
 	ret
+	.p2align 4,,10
+	.p2align 3
+.L13:
+	.cfi_restore_state
+	popl	%ebx
+	.cfi_remember_state
+	.cfi_restore 3
+	.cfi_def_cfa_offset 12
+	movl	$2147483647, %eax
+	popl	%esi
+	.cfi_restore 6
+	.cfi_def_cfa_offset 8
+	popl	%edi
+	.cfi_restore 7
+	.cfi_def_cfa_offset 4
+	ret
+	.p2align 4,,10
+	.p2align 3
+.L22:
+	.cfi_restore_state
+	movsbl	1(%ecx), %edx
+	movl	$-1, %edi
+	addl	$1, %ecx
+	jmp	.L6
+	.cfi_endproc
+.LFE0:
+	.size	myatoi, .-myatoi
+	.ident	"GCC: (Ubuntu 7.5.0-3ubuntu1~18.04) 7.5.0"
+	.section	.note.GNU-stack,"",@progbits
