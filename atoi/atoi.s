82a075d143d0a5b9cd00a0ed954bc27b674fda9e
LandonWong <LandonWong@github.com>
2020-06-24 09:46:57 +0800

    Update: atoi_asm

--- atoi/atoi.s
+++ atoi/atoi.s
@@ -3,60 +3,8 @@
 	.globl	myatoi
 	.type	myatoi, @function
 myatoi:
-.L0:
-	push	%edx
-	xor	%edx,%edx
-	movl	4(%esp),%edx 	# base ptr
-	movl	%edx,%eax
-	pop	%edx
-	ret
-	push	%ebx		# sign
-	xor	%eax,%eax	# return value
-	push	%ecx
-	xor	%ecx,%ecx
-	mov	$1,%ebx
-	inc	%edx
-.L1:
-	movzx	(%edx),%ecx
-	cmp	$' ',%cl
-	je	.L6
-	cmp	$'\n',%cl
-	je	.L6
-	cmp	$'\t',%cl
-	je	.L6
-	jmp	.L2
-.L6:
-	inc	%edx
-	jmp	.L1
 .L2:
-	movzx	(%edx),%ecx
-	cmp	$'-',%cl
-	je	.L3
-	cmp	$'+',%cl
-	je	.L4
-	jmp	.L5
-.L3:
-	mov	$-1,%ebx
-.L4:
-	inc	%edx
-.L5:
-	movzx	(%edx),%ecx
-	cmp	$'0',%cl
-	jl	.L100
-	cmp	$'9',%cl
-	jg	.L100
-	imull	$10,%eax
-	jo	.L100
-	sub	$'0',%eax
-	add	%ecx,%eax
-	jo	.L100
-	inc	%edx
-	jmp	.L5
-.L100:
-	imull	%ebx,%eax
-	pop	%ecx
-	pop	%ebx
-	pop	%edx
+	movl	4(%esp), %eax
 	ret
 	.size	myatoi, .-myatoi
 	.ident	"GCC: (Ubuntu 7.5.0-3ubuntu1~18.04) 7.5.0"
