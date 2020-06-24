6ac1f8b57f801fc130d3a00abb30f1311364bf26
LandonWong <LandonWong@github.com>
2020-06-24 09:28:00 +0800

    Update: atoi_asm

--- atoi/atoi.c
+++ atoi/atoi.c
@@ -1,6 +1,4 @@
 int
 myatoi(const char *nptr){
-	while(*nptr == '\n' || *nptr == ' ' || *nptr == '\t')
-		nptr++;
 L:	return *nptr;
 }
--- atoi/atoi.s
+++ atoi/atoi.s
@@ -5,23 +5,9 @@
 myatoi:
 .LFB0:
 	.cfi_startproc
-	movl	4(%esp), %edx
-	movzbl	(%edx), %eax
-	leal	-9(%eax), %ecx
-	cmpb	$1, %cl
-	jbe	.L5
-	cmpb	$32, %al
-	jne	.L2
-.L5:
-	addl	$1, %edx
-	movzbl	(%edx), %eax
-	leal	-9(%eax), %ecx
-	cmpb	$1, %cl
-	jbe	.L5
-	cmpb	$32, %al
-	je	.L5
 .L2:
-	movsbl	%al, %eax
+	movl	4(%esp), %eax
+	movsbl	(%eax), %eax
 	ret
 	.cfi_endproc
 .LFE0:
