4d5f9223ff7fc0e078688fded0a1581d0af95e2a
LandonWong <LandonWong@github.com>
2020-06-24 08:12:21 +0800

    Update: atoi_asm

--- atoi/atoi.s
+++ atoi/atoi.s
@@ -1,55 +1,78 @@
+	.file	"atoi.c"
 	.text
 	.globl	myatoi
 	.type	myatoi, @function
 myatoi:
 .LFB0:
+	.cfi_startproc
 	pushl	%esi
+	.cfi_def_cfa_offset 8
+	.cfi_offset 6, -8
 	pushl	%ebx
+	.cfi_def_cfa_offset 12
+	.cfi_offset 3, -12
 	movl	12(%esp), %edx
 	movzbl	(%edx), %ecx
 	leal	-9(%ecx), %eax
 	cmpb	$1, %al
-	jbe	.L13
+	jbe	.L15
 	cmpb	$32, %cl
 	jne	.L2
-.L13:
+.L15:
 	addl	$1, %edx
 	movzbl	(%edx), %ecx
 	leal	-9(%ecx), %eax
 	cmpb	$1, %al
-	jbe	.L13
+	jbe	.L15
 	cmpb	$32, %cl
-	je	.L13
+	je	.L15
 .L2:
 	cmpb	$45, %cl
-	je	.L15
+	je	.L17
 	movl	$1, %esi
 	cmpb	$43, %cl
-	je	.L16
+	je	.L18
 .L6:
+	movzbl	(%edx), %eax
+	leal	-48(%eax), %ecx
+	cmpb	$9, %cl
+	ja	.L13
+	movsbl	%al, %eax
+	leal	-48(%eax), %eax
+.L10:
+	addl	$1, %edx
 	movzbl	(%edx), %ecx
 	leal	-48(%ecx), %ebx
-	movl	$0, %eax
 	cmpb	$9, %bl
-	ja	.L8
-.L7:
+	ja	.L9
 	leal	(%eax,%eax,4), %eax
 	movsbl	%cl, %ecx
 	leal	-48(%ecx,%eax,2), %eax
-	addl	$1, %edx
-	movzbl	(%edx), %ecx
-	leal	-48(%ecx), %ebx
-	cmpb	$9, %bl
-	jbe	.L7
-.L8:
-	imull	%esi, %eax
-	popl	%ebx
-	popl	%esi
-	ret
-.L15:
+	cmpl	$458752, %eax
+	jle	.L10
+	movl	$2147483647, %eax
+	jmp	.L1
+.L17:
 	addl	$1, %edx
 	movl	$-1, %esi
 	jmp	.L6
-.L16:
+.L18:
 	addl	$1, %edx
 	jmp	.L6
+.L13:
+	movl	$0, %eax
+.L9:
+	imull	%esi, %eax
+.L1:
+	popl	%ebx
+	.cfi_restore 3
+	.cfi_def_cfa_offset 8
+	popl	%esi
+	.cfi_restore 6
+	.cfi_def_cfa_offset 4
+	ret
+	.cfi_endproc
+.LFE0:
+	.size	myatoi, .-myatoi
+	.ident	"GCC: (Ubuntu 7.5.0-3ubuntu1~18.04) 7.5.0"
+	.section	.note.GNU-stack,"",@progbits
