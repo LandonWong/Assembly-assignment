425ad3c2ff33f4f76c56340333caef033da2c4dc
LandonWong <LandonWong@github.com>
2020-06-24 11:35:36 +0800

    Update: memcpy_asm

--- memcpy/main.s
+++ memcpy/main.s
@@ -1,72 +1,69 @@
 	.file	"main.c"
 	.text
-	.comm	src,4,4
-	.comm	dest,4,4
 	.globl	InitSrcArea
 	.type	InitSrcArea, @function
 InitSrcArea:
-	pushl	%ebp
-	movl	%esp, %ebp
+	pushl	%esi
 	pushl	%ebx
-	subl	$20, %esp
+	subl	$16, %esp
 	call	__x86.get_pc_thunk.bx
 	addl	$_GLOBAL_OFFSET_TABLE_, %ebx
-	subl	$12, %esp
+	movl	28(%esp), %esi
 	pushl	$1000
 	call	malloc@PLT
+	movl	src@GOT(%ebx), %edx
+	movl	%eax, (%edx)
 	addl	$16, %esp
-	movl	%eax, %edx
-	movl	src@GOT(%ebx), %eax
-	movl	%edx, (%eax)
-	movl	src@GOT(%ebx), %eax
-	movl	(%eax), %eax
-	movl	%eax, -12(%ebp)
-	jmp	.L2
+	testl	%esi, %esi
+	je	.L1
 .L3:
-	movl	8(%ebp), %eax
-	movl	%eax, %edx
-	movl	-12(%ebp), %eax
-	movb	%dl, (%eax)
-	subl	$1, 8(%ebp)
-	addl	$1, -12(%ebp)
-.L2:
-	cmpl	$0, 8(%ebp)
+	movl	%esi, %ecx
+	movb	%cl, (%eax)
+	addl	$1, %eax
+	subl	$1, %esi
 	jne	.L3
-	nop
-	movl	-4(%ebp), %ebx
-	leave
+.L1:
+	addl	$4, %esp
+	popl	%ebx
+	popl	%esi
 	ret
 	.size	InitSrcArea, .-InitSrcArea
 	.globl	check
 	.type	check, @function
 check:
-	pushl	%ebp
-	movl	%esp, %ebp
-	call	__x86.get_pc_thunk.ax
-	addl	$_GLOBAL_OFFSET_TABLE_, %eax
-	jmp	.L5
+	pushl	%ebx
+	movl	8(%esp), %eax
+	movl	12(%esp), %edx
+	movl	16(%esp), %ecx
+	testl	%ecx, %ecx
+	je	.L10
+	movzbl	(%edx), %ebx
+	cmpb	%bl, (%eax)
+	jne	.L11
+	addl	%eax, %ecx
 .L8:
-	movl	8(%ebp), %eax
-	movzbl	(%eax), %edx
-	movl	12(%ebp), %eax
-	movzbl	(%eax), %eax
-	cmpb	%al, %dl
-	je	.L6
+	addl	$1, %eax
+	addl	$1, %edx
+	cmpl	%eax, %ecx
+	je	.L14
+	movzbl	(%edx), %ebx
+	cmpb	%bl, (%eax)
+	je	.L8
 	movl	$0, %eax
-	jmp	.L7
-.L6:
-	addl	$1, 8(%ebp)
-	addl	$1, 12(%ebp)
-	subl	$1, 16(%ebp)
-.L5:
-	cmpl	$0, 16(%ebp)
-	jne	.L8
+	jmp	.L6
+.L14:
 	movl	$1, %eax
-.L7:
-	popl	%ebp
+.L6:
+	popl	%ebx
 	ret
+.L10:
+	movl	$1, %eax
+	jmp	.L6
+.L11:
+	movl	$0, %eax
+	jmp	.L6
 	.size	check, .-check
-	.section	.rodata
+	.section	.rodata.str1.1,"aMS",@progbits,1
 .LC0:
 	.string	"pass"
 .LC1:
@@ -75,6 +72,7 @@ check:
 	.string	"1 Test src align %d, %s.\n"
 .LC3:
 	.string	"2 Test dest align %d, %s.\n"
+	.section	.rodata.str1.4,"aMS",@progbits,1
 	.align 4
 .LC4:
 	.string	"3 Test src & dest align %d, %s.\n"
@@ -87,191 +85,157 @@ main:
 	pushl	-4(%ecx)
 	pushl	%ebp
 	movl	%esp, %ebp
+	pushl	%edi
 	pushl	%esi
 	pushl	%ebx
 	pushl	%ecx
-	subl	$28, %esp
+	subl	$36, %esp
 	call	__x86.get_pc_thunk.bx
 	addl	$_GLOBAL_OFFSET_TABLE_, %ebx
-	movl	$0, -28(%ebp)
-	subl	$12, %esp
 	pushl	$1000
 	call	malloc@PLT
-	addl	$16, %esp
-	movl	%eax, %edx
-	movl	dest@GOT(%ebx), %eax
-	movl	%edx, (%eax)
-	subl	$12, %esp
-	pushl	$1000
+	movl	dest@GOT(%ebx), %edx
+	movl	%eax, (%edx)
+	movl	$1000, (%esp)
 	call	InitSrcArea
 	addl	$16, %esp
-	movl	$0, -28(%ebp)
-	jmp	.L10
-.L13:
+	movl	$0, %esi
 	movl	src@GOT(%ebx), %eax
-	movl	(%eax), %eax
-	movl	-28(%ebp), %edx
-	addl	$16, %edx
-	addl	%eax, %edx
+	movl	%eax, -28(%ebp)
 	movl	dest@GOT(%ebx), %eax
-	movl	(%eax), %eax
+	movl	%eax, -32(%ebp)
+.L17:
+	leal	16(%esi), %edi
 	subl	$4, %esp
 	pushl	$256
-	pushl	%edx
+	movl	-28(%ebp), %ecx
+	movl	%edi, %eax
+	addl	(%ecx), %eax
 	pushl	%eax
+	movl	-32(%ebp), %eax
+	pushl	(%eax)
 	call	mymemcpy@PLT
-	addl	$16, %esp
-	movl	src@GOT(%ebx), %eax
-	movl	(%eax), %eax
-	movl	-28(%ebp), %edx
-	addl	$16, %edx
-	addl	%eax, %edx
-	movl	dest@GOT(%ebx), %eax
-	movl	(%eax), %eax
-	subl	$4, %esp
+	addl	$12, %esp
 	pushl	$256
-	pushl	%edx
-	pushl	%eax
+	movl	-28(%ebp), %ecx
+	addl	(%ecx), %edi
+	pushl	%edi
+	movl	-32(%ebp), %eax
+	pushl	(%eax)
 	call	check
 	addl	$16, %esp
 	cmpl	$1, %eax
-	jne	.L11
 	leal	.LC0@GOTOFF(%ebx), %eax
-	jmp	.L12
-.L11:
-	leal	.LC1@GOTOFF(%ebx), %eax
-.L12:
-	subl	$4, %esp
+	leal	.LC1@GOTOFF(%ebx), %edx
+	cmovne	%edx, %eax
 	pushl	%eax
-	pushl	-28(%ebp)
+	pushl	%esi
 	leal	.LC2@GOTOFF(%ebx), %eax
 	pushl	%eax
-	call	printf@PLT
+	pushl	$1
+	call	__printf_chk@PLT
+	addl	$1, %esi
 	addl	$16, %esp
-	addl	$1, -28(%ebp)
-.L10:
-	cmpl	$3, -28(%ebp)
-	jle	.L13
-	movl	$0, -28(%ebp)
-	jmp	.L14
-.L17:
+	cmpl	$4, %esi
+	jne	.L17
+	movl	$0, %esi
 	movl	src@GOT(%ebx), %eax
-	movl	(%eax), %eax
-	movl	dest@GOT(%ebx), %edx
-	movl	(%edx), %edx
-	movl	-28(%ebp), %ecx
-	addl	$16, %ecx
-	addl	%ecx, %edx
+	movl	%eax, -28(%ebp)
+	movl	dest@GOT(%ebx), %eax
+	movl	%eax, -32(%ebp)
+.L19:
+	leal	16(%esi), %edi
 	subl	$4, %esp
 	pushl	$256
-	pushl	%eax
+	movl	-28(%ebp), %eax
+	pushl	(%eax)
+	movl	-32(%ebp), %ecx
+	movl	%edi, %edx
+	addl	(%ecx), %edx
 	pushl	%edx
 	call	mymemcpy@PLT
-	addl	$16, %esp
-	movl	src@GOT(%ebx), %eax
-	movl	(%eax), %eax
-	movl	dest@GOT(%ebx), %edx
-	movl	(%edx), %edx
-	movl	-28(%ebp), %ecx
-	addl	$16, %ecx
-	addl	%ecx, %edx
-	subl	$4, %esp
+	addl	$12, %esp
 	pushl	$256
-	pushl	%eax
-	pushl	%edx
+	movl	-28(%ebp), %eax
+	pushl	(%eax)
+	movl	-32(%ebp), %ecx
+	addl	(%ecx), %edi
+	pushl	%edi
 	call	check
 	addl	$16, %esp
 	cmpl	$1, %eax
-	jne	.L15
 	leal	.LC0@GOTOFF(%ebx), %eax
-	jmp	.L16
-.L15:
-	leal	.LC1@GOTOFF(%ebx), %eax
-.L16:
-	subl	$4, %esp
+	leal	.LC1@GOTOFF(%ebx), %edx
+	cmovne	%edx, %eax
 	pushl	%eax
-	pushl	-28(%ebp)
+	pushl	%esi
 	leal	.LC3@GOTOFF(%ebx), %eax
 	pushl	%eax
-	call	printf@PLT
+	pushl	$1
+	call	__printf_chk@PLT
+	addl	$1, %esi
 	addl	$16, %esp
-	addl	$1, -28(%ebp)
-.L14:
-	cmpl	$3, -28(%ebp)
-	jle	.L17
-	movl	$0, -28(%ebp)
-	jmp	.L18
-.L21:
+	cmpl	$4, %esi
+	jne	.L19
+	movl	$0, %esi
 	movl	src@GOT(%ebx), %eax
-	movl	(%eax), %eax
-	movl	-28(%ebp), %edx
-	addl	$32, %edx
-	addl	%eax, %edx
+	movl	%eax, -28(%ebp)
 	movl	dest@GOT(%ebx), %eax
-	movl	(%eax), %eax
-	movl	-28(%ebp), %ecx
-	movl	$16, %esi
-	subl	%ecx, %esi
-	movl	%esi, %ecx
-	addl	%ecx, %eax
+	movl	%eax, -32(%ebp)
+.L21:
+	leal	32(%esi), %edi
+	movl	$16, %eax
+	subl	%esi, %eax
+	movl	%eax, %ecx
 	subl	$4, %esp
 	pushl	$256
-	pushl	%edx
+	movl	-28(%ebp), %edx
+	movl	%edi, %eax
+	addl	(%edx), %eax
+	pushl	%eax
+	movl	-32(%ebp), %edx
+	movl	%ecx, -36(%ebp)
+	movl	%ecx, %eax
+	addl	(%edx), %eax
 	pushl	%eax
 	call	mymemcpy@PLT
-	addl	$16, %esp
-	movl	src@GOT(%ebx), %eax
-	movl	(%eax), %eax
-	movl	-28(%ebp), %edx
-	addl	$32, %edx
-	addl	%eax, %edx
-	movl	dest@GOT(%ebx), %eax
-	movl	(%eax), %eax
-	movl	-28(%ebp), %ecx
-	movl	$16, %esi
-	subl	%ecx, %esi
-	movl	%esi, %ecx
-	addl	%ecx, %eax
-	subl	$4, %esp
+	addl	$12, %esp
 	pushl	$256
-	pushl	%edx
+	movl	-28(%ebp), %edx
+	addl	(%edx), %edi
+	pushl	%edi
+	movl	-32(%ebp), %edx
+	movl	-36(%ebp), %eax
+	addl	(%edx), %eax
 	pushl	%eax
 	call	check
 	addl	$16, %esp
 	cmpl	$1, %eax
-	jne	.L19
 	leal	.LC0@GOTOFF(%ebx), %eax
-	jmp	.L20
-.L19:
-	leal	.LC1@GOTOFF(%ebx), %eax
-.L20:
-	subl	$4, %esp
+	leal	.LC1@GOTOFF(%ebx), %edx
+	cmovne	%edx, %eax
 	pushl	%eax
-	pushl	-28(%ebp)
+	pushl	%esi
 	leal	.LC4@GOTOFF(%ebx), %eax
 	pushl	%eax
-	call	printf@PLT
+	pushl	$1
+	call	__printf_chk@PLT
+	addl	$1, %esi
 	addl	$16, %esp
-	addl	$1, -28(%ebp)
-.L18:
-	cmpl	$3, -28(%ebp)
-	jle	.L21
+	cmpl	$4, %esi
+	jne	.L21
 	movl	$0, %eax
-	leal	-12(%ebp), %esp
+	leal	-16(%ebp), %esp
 	popl	%ecx
 	popl	%ebx
 	popl	%esi
+	popl	%edi
 	popl	%ebp
 	leal	-4(%ecx), %esp
 	ret
 	.size	main, .-main
-	.section	.text.__x86.get_pc_thunk.ax,"axG",@progbits,__x86.get_pc_thunk.ax,comdat
-	.globl	__x86.get_pc_thunk.ax
-	.hidden	__x86.get_pc_thunk.ax
-	.type	__x86.get_pc_thunk.ax, @function
-__x86.get_pc_thunk.ax:
-	movl	(%esp), %eax
-	ret
+	.comm	dest,4,4
+	.comm	src,4,4
 	.section	.text.__x86.get_pc_thunk.bx,"axG",@progbits,__x86.get_pc_thunk.bx,comdat
 	.globl	__x86.get_pc_thunk.bx
 	.hidden	__x86.get_pc_thunk.bx
--- memcpy/memcpy.s
+++ memcpy/memcpy.s
@@ -3,22 +3,8 @@
 	.globl	mymemcpy
 	.type	mymemcpy, @function
 mymemcpy:
-	pushl	%ebp
-	movl	%esp, %ebp
-	call	__x86.get_pc_thunk.ax
-	addl	$_GLOBAL_OFFSET_TABLE_, %eax
-	addl	$1, 12(%ebp)
-	subl	$1, 16(%ebp)
-	movl	8(%ebp), %eax
-	popl	%ebp
+	movl	4(%esp), %eax
 	ret
 	.size	mymemcpy, .-mymemcpy
-	.section	.text.__x86.get_pc_thunk.ax,"axG",@progbits,__x86.get_pc_thunk.ax,comdat
-	.globl	__x86.get_pc_thunk.ax
-	.hidden	__x86.get_pc_thunk.ax
-	.type	__x86.get_pc_thunk.ax, @function
-__x86.get_pc_thunk.ax:
-	movl	(%esp), %eax
-	ret
 	.ident	"GCC: (Ubuntu 7.5.0-3ubuntu1~18.04) 7.5.0"
 	.section	.note.GNU-stack,"",@progbits
