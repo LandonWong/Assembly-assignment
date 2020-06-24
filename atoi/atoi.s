2b63504afa497f24d750b647304942e997640b82
LandonWong <LandonWong@github.com>
2020-06-24 09:00:34 +0800

    Update: atoi_clean

--- atoi/atoi.s
+++ atoi/atoi.s
@@ -1,16 +1,52 @@
-	.file	"atoi.c"
 	.text
-	.p2align 4,,15
 	.globl	myatoi
 	.type	myatoi, @function
 myatoi:
-.LFB0:
-	.cfi_startproc
-	movl	4(%esp), %eax
-	movsbl	(%eax), %eax
+.L0:
+	push	%edx
+	movl	4(%esp), %edx 	# base ptr
+	push	%ebx		# sign
+	xor	%eax,%eax	# return value
+	push	%ecx
+	xor	%cl,%cl
+	mov	$1,%ebx
+.L1:
+	movb	(%edx),%cl
+	cmp	$' ',%cl
+	jne	.L2
+	cmp	$'\n',%cl
+	jne	.L2
+	cmp	$'\t',%cl
+	jne	.L2
+	inc	%edx
+	jmp	.L1
+.L2:
+	movb	(%edx),%cl
+	cmp	$'-',%cl
+	je	.L3
+	cmp	$'+',%cl
+	je	.L4
+	jmp	.L5
+.L3:
+	mov	$-1,%ebx
+.L4:
+	inc	%edx
+.L5:
+	movb	(%edx),%cl
+	cmp	$'0',%cl
+	jl	.L100
+	cmp	$'9',%cl
+	jg	.L100
+	imull	$10,%eax
+	jo	.L100
+	sub	$'0',%eax
+	add	%cl,%eax
+	jo	.L100
+	inc	%edx
+	jmp	.L5
+.L100:
+	imull	%ebx,%eax
+	pop	%ecx
+	pop	%ebx
+	pop	%edx
 	ret
-	.cfi_endproc
-.LFE0:
-	.size	myatoi, .-myatoi
-	.ident	"GCC: (Ubuntu 7.5.0-3ubuntu1~18.04) 7.5.0"
-	.section	.note.GNU-stack,"",@progbits
