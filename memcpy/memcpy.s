5014b02cfd7962ae0973ce30eaa129cc97180a85
LandonWong <LandonWong@github.com>
2020-06-25 13:56:26 +0800

    Update: memcpy_asm

--- memcpy/memcpy.s
+++ memcpy/memcpy.s
@@ -36,6 +36,8 @@ mymemcpy:
 	mov	$1,%rcx
 	jmp	.L7
 .L4:
+	cmp	$64,%rbx
+	jge	.L25
 	jmp	.L10
 .L6:
 	sub	%rcx,%rbx
@@ -57,6 +59,39 @@ mymemcpy:
 	sub	%r8,%rbx
 	cld
 	rep	movsq
+	jmp	.L2
+.L25:
+	push	%r9
+	push	%r10
+	push	%r11
+	push	%r12
+.L11:
+	movq	(%rsi),%r9
+	movq	8(%rsi),%r10
+	movq	16(%rsi),%r11
+	movq	24(%rsi),%r12
+	movq	32(%rsi),%mm0
+	movq	40(%rsi),%mm1
+	movq	48(%rsi),%mm2
+	movq	56(%rsi),%mm3
+	movq	%r9,(%rax,%r8)
+	movq	%r10,8(%rax,%r8)
+	movq	%r11,16(%rax,%r8)
+	movq	%r12,24(%rax,%r8)
+	movq	%mm0,32(%rax,%r8)
+	movq	%mm1,40(%rax,%r8)
+	movq	%mm2,48(%rax,%r8)
+	movq	%mm3,56(%rax,%r8)
+	add	$64,%r8
+	add	$64,%rsi
+	sub	$64,%rbx
+	cmp	$64,%rbx
+	jge	.L11
+	pop	%r12
+	pop	%r11
+	pop	%r10
+	pop	%r9
+	cmp	$64,%rbx
 	jmp	.L2
 .L1:
 	pop	%rdx
