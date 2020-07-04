6ebad1eb12ef34c773c381db364982d5fcab308e
LandonWong <LandonWong@github.com>
2020-07-04 19:21:36 +0800

    Update: freq_compile

--- freq/freq.c
+++ freq/freq.c
@@ -2,7 +2,7 @@
 #include <sys/time.h>
 void
 main(){
-	int cycle_1_h,cycle_1_l,cycle_2_h,cycle_2_l;
+	unsigned long cycle_1_h,cycle_1_l,cycle_2_h,cycle_2_l;
 	long time;
 	long cycle_1,cycle_2;
 	struct timeval tv1, tv2;
@@ -20,8 +20,8 @@ main(){
 		"mov	%edx,cycle_2_h\n\t"
 	);
 	gettimeofday(&tv2, NULL);
-	cycle_1 = cycle_1_h << 32 | cycle_1_l;
-	cycle_2 = cycle_2_h << 32 | cycle_2_l;
+	cycle_1 = (cycle_1_h << 32) | cycle_1_l;
+	cycle_2 = (cycle_2_h << 32) | cycle_2_l;
 	time = (tv2.tv_sec - tv1.tv_sec) * 1000000 + (tv2.tv_usec - tv1.tv_usec);
 	cpufreq = (cycle_2 - cycle_1) / time;
 	printf("The cpu frequency is %.2f MHz\n", cpufreq);
