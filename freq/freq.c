db79a1c1c058836be394991ccd1d292ff316cae5
LandonWong <LandonWong@github.com>
2020-07-04 22:30:12 +0800

    Update: freq_compile

--- freq/freq.c
+++ freq/freq.c
@@ -3,28 +3,27 @@
 void
 main(){
 	struct timeval tv1, tv2;
-	long time;
+	long time,cycle_1_l,cycle_2_l,cycle_1_h,cycle_2_h;
 	long long cycle_1,cycle_2;
 	double cpufreq;
 	while(1){
 		gettimeofday(&tv1, NULL);
 		asm volatile(
 			"rdtscp\n\t"
-			"shl	$32,%%rdx\n\t"
-			"or	%%rax,%%rdx\n\t"
-			"mov	%%rdx,%1\n\t"
-			"mov	$0x8fffffff,%%rcx\n"
+			"mov	%%rdx,%3\n\t"
+			"mov	%%rax,%2\n\t"
+			"mov	$0x7fffffff,%%rcx\n"
 			"L1:\n\t"
 			"xor	%%ecx,%%eax\n\t"
 			"inc	%%eax\n\t"
 			"dec	%%rcx\n\t"
 			"jnz	L1\n\t"
 			"rdtscp\n\t"
-			"shl	$32,%%rdx\n\t"
-			"or	%%rdx,%%rax\n\t"
-			: "=a" (cycle_2), "=r" (cycle_1)
+			: "=a" (cycle_2_l), "=d" (cycle_2_h), "=r" (cycle_1_l), "=r" (cycle_1_h)
 		);
 		gettimeofday(&tv2, NULL);
+		cycle_1 = (cycle_1_h << 32) | (cycle_1_l);
+		cycle_2 = (cycle_2_h << 32) | (cycle_2_l);
 		time = (tv2.tv_sec - tv1.tv_sec) * 1000000 + (tv2.tv_usec - tv1.tv_usec);
 		cpufreq = (cycle_2 - cycle_1) * 1.0 / (time * 1.0);
 		printf("The cpu frequency is %.3f MHz, time = %d us, cycle = %lu\n",cpufreq,time,cycle_2 - cycle_1);
--- freq/freq.s
+++ freq/freq.s
@@ -8,57 +8,59 @@
 	.globl	main
 	.type	main, @function
 main:
+	pushq	%r15
 	pushq	%r14
 	pushq	%r13
 	pushq	%r12
 	pushq	%rbp
 	pushq	%rbx
-	subq	$48, %rsp
+	subq	$56, %rsp
 	movq	%fs:40, %rax
 	movq	%rax, 40(%rsp)
 	xorl	%eax, %eax
-	movq	%rsp, %r14
-	leaq	16(%rsp), %r13
-	leaq	.LC0(%rip), %r12
+	movq	%rsp, %r15
+	leaq	16(%rsp), %r14
 .L2:
 	movl	$0, %esi
-	movq	%r14, %rdi
+	movq	%r15, %rdi
 	call	gettimeofday@PLT
 #APP
 # 11 "freq.c" 1
 	rdtscp
-	shl	$32,%rdx
-	or	%rax,%rdx
 	mov	%rdx,%rbp
-	mov	$0x8fffffff,%rcx
+	mov	%rax,%r12
+	mov	$0x7fffffff,%rcx
 L1:
 	xor	%ecx,%eax
 	inc	%eax
 	dec	%rcx
 	jnz	L1
 	rdtscp
-	shl	$32,%rdx
-	or	%rdx,%rax
 	
 # 0 "" 2
 #NO_APP
-	movq	%rax, %rbx
+	movq	%rdx, %rbx
+	movq	%rax, %r13
 	movl	$0, %esi
-	movq	%r13, %rdi
+	movq	%r14, %rdi
 	call	gettimeofday@PLT
 	movq	16(%rsp), %rdx
 	subq	(%rsp), %rdx
 	imulq	$1000000, %rdx, %rdx
 	addq	24(%rsp), %rdx
 	subq	8(%rsp), %rdx
-	subq	%rbp, %rbx
+	salq	$32, %rbx
 	movq	%rbx, %rcx
+	orq	%r13, %rcx
+	salq	$32, %rbp
+	orq	%rbp, %r12
+	subq	%r12, %rcx
 	pxor	%xmm0, %xmm0
-	cvtsi2sdq	%rbx, %xmm0
+	cvtsi2sdq	%rcx, %xmm0
 	pxor	%xmm1, %xmm1
 	cvtsi2sdq	%rdx, %xmm1
 	divsd	%xmm1, %xmm0
-	movq	%r12, %rsi
+	leaq	.LC0(%rip), %rsi
 	movl	$1, %edi
 	movl	$1, %eax
 	call	__printf_chk@PLT
